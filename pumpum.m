7334867effe09f58a893997f0e072bae633bd049
nemeid <ek.yastrebova@gmail.com>
2017-02-22 12:22:03 +0400

    first commit

---
+++ pumpum.m
@@ -0,0 +1,14 @@
+ 
+
+[t,i1,i1]=intersect(t1,t2);
+Undefined function or variable 't1'.
+ 
+t
+Undefined function or variable 't'.
+ 
+figure(2);plot(t)
+Undefined function or variable 't'.
+ 
+plot(t1,S1,t2,S2)
+Undefined function or variable 't1'.
+ 
